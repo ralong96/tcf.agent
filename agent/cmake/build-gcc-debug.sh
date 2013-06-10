88c86639f1f4187118428cba33c14ea1998f8e2f
Martin Oberhuber <martin.oberhuber@windriver.com>
2013-06-10 16:45:03 +0200

    Bug 410348 - [releng][agent] Update Copyright Dates for the TCF Agent
    Resolved Conflicts after cherry-picking from master:
    	agent/cmake/build-gcc-debug.sh
    	agent/cmake/build-gcc-release.sh

--- agent/cmake/build-gcc-debug.sh
+++ agent/cmake/build-gcc-debug.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+#*******************************************************************************
+# Copyright (c) 2011, 2013 Wind River Systems, Inc. and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# and Eclipse Distribution License v1.0 which accompany this distribution.
+# The Eclipse Public License is available at
+# http://www.eclipse.org/legal/epl-v10.html
+# and the Eclipse Distribution License is available at
+# http://www.eclipse.org/org/documents/edl-v10.php.
+# You may elect to redistribute this code under either of these licenses.
+#
+# Contributors:
+#     Wind River Systems - initial API and implementation
+#*******************************************************************************
 [ -d gcc-debug ] || mkdir gcc-debug
 cd gcc-debug
 [ -f Makefile ] || cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" ../..
--- agent/cmake/build-msvc-release.bat
+++ agent/cmake/build-msvc-release.bat
@@ -1,3 +1,17 @@
+@rem ***************************************************************************
+@rem Copyright (c) 2011 Wind River Systems, Inc. and others.
+@rem All rights reserved. This program and the accompanying materials
+@rem are made available under the terms of the Eclipse Public License v1.0
+@rem and Eclipse Distribution License v1.0 which accompany this distribution.
+@rem The Eclipse Public License is available at
+@rem http://www.eclipse.org/legal/epl-v10.html
+@rem and the Eclipse Distribution License is available at
+@rem http://www.eclipse.org/org/documents/edl-v10.php.
+@rem You may elect to redistribute this code under either of these licenses.
+@rem
+@rem Contributors:
+@rem     Wind River Systems - initial API and implementation
+@rem ***************************************************************************
 @echo off
 REM The Visual C++ compiler must be in your path
 REM This is handled automatically with CDT's Visual C++ integration
