ralong96/tcf.agent

commit f6cfb9cb11b0a02fa24e4548faa78fcd0268a4f7

TCF Agent: new field in MemoryRegion: channel. It allows to know which client has submitted the region info.

agent/tcf/services/memorymap.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2013 Wind River Systems, Inc. and others.
+ * Copyright (c) 2009, 2014 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
@@ -148,7 +148,8 @@ static void update_context_client_map(Context * ctx) {
                         y->flags == x->flags &&
                         str_equ(y->file_name, x->file_name) &&
                         str_equ(y->sect_name, x->sect_name) &&
-                        str_equ(y->query, x->query);
+                        str_equ(y->query, x->query) &&
+                        y->channel == x->channel;
                     if (equ) {
                         MemoryRegionAttribute * ax = x->attrs;
                         MemoryRegionAttribute * ay = y->attrs;
@@ -197,6 +198,7 @@ static void update_context_client_map(Context * ctx) {
                             ax = ax->next;
                         }
                     }
+                    y->channel = m->channel;
                 }
             }
         }

agent/tcf/framework/context.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2013 Wind River Systems, Inc. and others.
+ * Copyright (c) 2007, 2014 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
@@ -21,6 +21,7 @@
 #define D_context
 
 #include <tcf/config.h>
+#include <tcf/framework/channel.h>
 #include <tcf/framework/cpudefs.h>
 #include <tcf/framework/errors.h>
 #include <tcf/framework/sigsets.h>
@@ -141,6 +142,7 @@ struct MemoryRegion {
     char * query;                   /* If not NULL, the region is only part of the memory map for contexts matches the query */
     char * id;                      /* Region ID, not NULL only if the region info is submitted by a client */
     MemoryRegionAttribute * attrs;  /* Additional memory region attributes */
+    Channel * channel;              /* Not NULL if the region info is submitted by a client */
 };
 
 struct MemoryRegionAttribute {

commit 3d62e44b7c53410b9af06fee5c1151c6e561d0a2

Bug 455983 - Error introduced by commit f6cfb9cb11b0a02fa24e4548faa78fcd0268a4f7

agent/tcf/services/memorymap.c
@@ -149,7 +149,7 @@ static void update_context_client_map(Context * ctx) {
                         str_equ(y->file_name, x->file_name) &&
                         str_equ(y->sect_name, x->sect_name) &&
                         str_equ(y->query, x->query) &&
-                        y->channel == x->channel;
+                        y->channel == m->channel;
                     if (equ) {
                         MemoryRegionAttribute * ax = x->attrs;
                         MemoryRegionAttribute * ay = y->attrs;