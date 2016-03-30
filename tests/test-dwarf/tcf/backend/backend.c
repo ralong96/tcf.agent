5b6178c72356c44df920a3893a1cad2d3200949f
Eugene Tarassov <eugene.tarassov@xilinx.com>
2016-03-30 14:43:53 -0700

    TCF Tests: more DWARF reader tests

--- tests/test-dwarf/tcf/backend/backend.c
+++ tests/test-dwarf/tcf/backend/backend.c
@@ -911,7 +911,7 @@ static void loc_var_func(void * args, Symbol * sym) {
         if (errcmp(err, "Object is not available at this location") == 0) return;
         if (errcmp(err, "OP_fbreg: cannot read AT_frame_base") == 0) return;
         if (errcmp(err, "OP_implicit_pointer: invalid object reference") == 0) return;
-        if (errcmp(err, "Thread local storage access is not supported yet for machine type 20") == 0) return;
+        if (errcmp(err, "Thread local storage access is not supported yet for machine type") == 0) return;
         if (errcmp(err, "Division by zero in location") == 0) return;
         if (errcmp(err, "Cannot find loader debug") == 0) return;
         if (errcmp(err, "Cannot get TLS module ID") == 0) return;
