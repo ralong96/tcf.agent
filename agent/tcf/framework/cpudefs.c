1bc0abf2d2fae08c55e6f2bb8e737a4c85f8104a
Eugene Tarassov <eugenet@xilinx.com>
2017-12-25 12:01:12 -0800

    TCF Agent: fixed compilation warning: ‘%s’ directive output may be truncated ...

--- agent/tcf/framework/cpudefs.c
+++ agent/tcf/framework/cpudefs.c
@@ -166,24 +166,26 @@ int id2frame(const char * id, Context ** ctx, int * frame) {
 }
 
 const char * frame2id(Context * ctx, int frame) {
-    char id[256];
+    size_t l = strlen(ctx->id) + 20;
+    char * id = tmp_alloc(l);
     assert(frame >= 0);
-    snprintf(id, sizeof(id), "FP%d.%s", frame, ctx->id);
-    return tmp_strdup(id);
+    snprintf(id, l, "FP%d.%s", frame, ctx->id);
+    return id;
 }
 
 const char * register2id(Context * ctx, int frame, RegisterDefinition * reg) {
-    char id[256];
     RegisterDefinition * defs = get_reg_definitions(ctx);
+    size_t l = strlen(ctx->id) + 30;
+    char * id = tmp_alloc(l);
     assert(defs != NULL);
     assert(reg >= defs);
     if (frame < 0) {
-        snprintf(id, sizeof(id), "R%d.%s", (int)(reg - defs), ctx->id);
+        snprintf(id, l, "R%d.%s", (int)(reg - defs), ctx->id);
     }
     else {
-        snprintf(id, sizeof(id), "R%d@%d.%s", (int)(reg - defs), frame, ctx->id);
+        snprintf(id, l, "R%d@%d.%s", (int)(reg - defs), frame, ctx->id);
     }
-    return tmp_strdup(id);
+    return id;
 }
 
 int id2reg_num(const char * id, const char ** ctx_id, int * frame, unsigned * reg_num) {
